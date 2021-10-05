29224226f43bcd9e3bd951cbce30a26272f06ef5
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2021-10-05 15:28:18 +0200

    Mise en place du header et de la barre de recherche version desktop et mobile

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import { Recipe } from "./class/Recipe.js";
+
+function displayRecipes() {
+  fetch("./data/recipes.json")
+    .then((response) => response.json())
+    .then((data) => {
+      let recipes = data.recipes;
+      console.log(recipes);
+      for (let i in recipes) {
+        new Recipe(recipes[i]).createRecipeCard();
+      }
+    });
+}
+
+displayRecipes();
