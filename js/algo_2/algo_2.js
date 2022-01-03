ee0baaac48b7f4d767c33c2772635c916332f92a
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-01-03 15:17:51 +0100

    mise en place de l'algo 2 avec une double boucle

--- js/algo_2/algo_2.js
+++ js/algo_2/algo_2.js
@@ -1,28 +1,3 @@
-/*export function recipeFilter(elementMatch) {
-  console.log(elementMatch);
-  let recipeCards = document.querySelectorAll(".recipe-card");
-  recipeCards.forEach((recipeCard) => {
-    let allData = [];
-    let ingredientsData = recipeCard.dataset.filterIngredients;
-    let ustensilsData = recipeCard.dataset.filterUstensils;
-    let appliancesData = recipeCard.dataset.filterAppliances;
-    allData.push(
-      ingredientsData.split(","),
-      ustensilsData.split(","),
-      appliancesData.split(",")
-    );
-    let allDataString = allData.toString();
-    console.log(allDataString);
-    recipeCard.style.display = "block";
-    for (let i in elementMatch) {
-      if (allDataString.includes(elementMatch[i]) != true) {
-        recipeCard.style.display = "none";
-      }
-    }
-  });
-}
-*/
-
 export function recipeFilter(elementMatch) {
   console.log(elementMatch);
   let recipeCards = document.querySelectorAll(".recipe-card");
@@ -32,12 +7,19 @@ export function recipeFilter(elementMatch) {
     let ustensilsData = recipeCard.dataset.filterUstensils;
     let appliancesData = recipeCard.dataset.filterAppliances;
     allData.push(
-      ingredientsData.split(","),
-      ustensilsData.split(","),
-      appliancesData.split(",")
+      ingredientsData
+        .split(",")
+        .concat(ustensilsData.split(","))
+        .concat(appliancesData)
     );
-    let allDataString = allData.toString();
-
+    console.log(allData);
     recipeCard.style.display = "block";
+    for (let i in allData) {
+      for (let e in elementMatch) {
+        if (allData[i].indexOf(elementMatch[e]) === -1) {
+          recipeCard.style.display = "none";
+        }
+      }
+    }
   });
 }
