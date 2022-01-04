d8f91a340ecf1d7df4999bda8ef916e11fdf0bd6
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-01-04 22:23:47 +0100

    Fix algo 1 recherche par nom de recette

--- js/algo_1/algo_1.js
+++ js/algo_1/algo_1.js
@@ -1,16 +1,19 @@
 export function recipeFilter(elementMatch) {
   console.log(elementMatch);
   let recipeCards = document.querySelectorAll(".recipe-card");
+  console.log(recipeCards);
   recipeCards.forEach((recipeCard) => {
     let allData = [];
     console.log(allData);
     let ingredientsData = recipeCard.dataset.filterIngredients;
     let ustensilsData = recipeCard.dataset.filterUstensils;
     let appliancesData = recipeCard.dataset.filterAppliances;
+    let recipeName = recipeCard.dataset.filterName;
     allData.push(
       ingredientsData.split(","),
       ustensilsData.split(","),
-      appliancesData.split(",")
+      appliancesData.split(","),
+      recipeName
     );
     let allDataString = allData.toString();
     console.log(allDataString);
