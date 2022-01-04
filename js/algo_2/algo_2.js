d37f38982c79f278e57321a07af1214d89f410dc
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-01-04 22:15:06 +0100

    Modification de la fonction mainSearchBar

--- js/algo_2/algo_2.js
+++ js/algo_2/algo_2.js
@@ -1,3 +1,30 @@
+/*export function recipeFilter(elementMatch) {
+  console.log(elementMatch);
+  let recipeCards = document.querySelectorAll(".recipe-card");
+  recipeCards.forEach((recipeCard) => {
+    let allData = [];
+    console.log(allData);
+    let ingredientsData = recipeCard.dataset.filterIngredients;
+    let ustensilsData = recipeCard.dataset.filterUstensils;
+    let appliancesData = recipeCard.dataset.filterAppliances;
+    allData.push(
+      ingredientsData.split(","),
+      ustensilsData.split(","),
+      appliancesData.split(",")
+    );
+    let allDataString = allData.toString();
+    console.log(allDataString);
+    console.log(allDataString.includes(elementMatch));
+    recipeCard.style.display = "block";
+    for (let i in elementMatch) {
+      if (allDataString.includes(elementMatch[i]) != true) {
+        recipeCard.style.display = "none";
+      }
+    }
+  });
+}
+*/
+
 export function recipeFilter(elementMatch) {
   console.log(elementMatch);
   let recipeCards = document.querySelectorAll(".recipe-card");
@@ -12,10 +39,13 @@ export function recipeFilter(elementMatch) {
         .concat(ustensilsData.split(","))
         .concat(appliancesData)
     );
-    recipeCard.style.display = "block";
+    //recipeCard.style.display = "block";
     for (let i in allData) {
       for (let e in elementMatch) {
-        if (allData[i].indexOf(elementMatch[e]) === -1) {
+        if (
+          allData[i].indexOf(elementMatch[e]) === -1 &&
+          allData[i].includes(elementMatch) != true
+        ) {
           recipeCard.style.display = "none";
         }
       }
