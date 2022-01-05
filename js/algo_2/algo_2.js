fbff8472b5102e3f5a7042501d6003901e8805df
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-01-05 17:02:47 +0100

    AJout de la methode split pour transformer une string en tableau

--- js/algo_2/algo_2.js
+++ js/algo_2/algo_2.js
@@ -26,26 +26,31 @@
 */
 
 export function recipeFilter(elementMatch) {
-  console.log(elementMatch);
   let recipeCards = document.querySelectorAll(".recipe-card");
   recipeCards.forEach((recipeCard) => {
     let allData = [];
     let ingredientsData = recipeCard.dataset.filterIngredients;
     let ustensilsData = recipeCard.dataset.filterUstensils;
     let appliancesData = recipeCard.dataset.filterAppliances;
+    let recipeName = recipeCard.dataset.filterName;
     allData.push(
       ingredientsData
         .split(",")
         .concat(ustensilsData.split(","))
         .concat(appliancesData)
+        .concat(recipeName)
     );
-    //recipeCard.style.display = "block";
+    console.log(allData)
+    recipeCard.style.display = "block";
     for (let i in allData) {
+      console.log(allData[i])
       for (let e in elementMatch) {
-        if (
-          allData[i].indexOf(elementMatch[e]) === -1 &&
-          allData[i].includes(elementMatch) != true
-        ) {
+        console.log( elementMatch[e].split().concat())
+        /*let allDataString = allData[i].toString();
+        if (allData[i].indexOf(elementMatch[e]) === -1) {
+        }
+        */
+        if (allData[i].includes(elementMatch[e]) != true) {
           recipeCard.style.display = "none";
         }
       }
